0f2299c91becab4bdfd7451ac9fdbd784b514d99
gimtommang11 <gimtommang88@gmail.com>
2020-06-23 10:13:40 +0900

    같은숫자는 싫어 문제풀이

---
+++ Algorithm/noSameNumber.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      answer.push(arr[i]);
+    }
+  }
+}
+
+const arr = [4, 4, 4, 3, 3]
+solution(arr)
